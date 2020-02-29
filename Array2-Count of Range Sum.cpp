240865a0ae7c2b3773308a687006961712d09318
Nitesh Kumar Tiwari <niteshsoftwaredeveloper@gmail.com>
2020-02-29 19:19:00 +0530

    range count

--- Array2-Count of Range Sum.cpp
+++ Array2-Count of Range Sum.cpp
@@ -10,36 +10,41 @@ Range sum S(i, j) is defined as the sum of the elements in nums between indices 
 using namespace std;
 
 class Solution {
-    int mergeSort(vector<long>& prefix, int lower, int upper, int l, int r) {
-        if (r - l <= 1) return 0;
-
-        int mid = l + (r - l) / 2;
-        int count = mergeSort(prefix, lower, upper, l, mid);
-        count += mergeSort(prefix, lower, upper, mid, r);
-        int j = mid, k = mid;
-        for (int i = l; i < mid; i++) {
-            while (j < r && prefix[j] - prefix[i] < lower) j++;
-            while (k < r && prefix[k] - prefix[i] <= upper) k++;
-
-            count += k - j;
-        }
-
-        inplace_merge(prefix.begin() + l, prefix.begin() + mid, prefix.begin() + r);
-        return count;
-    }
-
    public:
     int countRangeSum(vector<int>& nums, int lower, int upper) {
-        int n = nums.size();
-        vector<long> prefixSum(n + 1, 0);
+        int size = nums.size();
+        if (size == 0) return 0;
+        vector<long> sums(size + 1, 0);
+        for (int i = 0; i < size; i++) sums[i + 1] = sums[i] + nums[i];
+        return help(sums, 0, size + 1, lower, upper);
+    }
 
-        for (int i = 0; i < n; i++)
-            prefixSum[i + 1] = prefixSum[i] + nums[i];
+    /*** [start, end)  ***/
+    int help(vector<long>& sums, int start, int end, int lower, int upper) {
+        /*** only-one-element, so the count-pair=0 ***/
+        if (end - start <= 1) return 0;
+        int mid = (start + end) / 2;
+        int count = help(sums, start, mid, lower, upper) + help(sums, mid, end, lower, upper);
 
-        return mergeSort(prefixSum, lower, upper, 0, n + 1);
+        int m = mid, n = mid, t = mid, len = 0;
+        /*** cache stores the sorted-merged-2-list ***/
+        /*** so we use the "len" to record the merged length ***/
+        vector<long> cache(end - start, 0);
+        for (int i = start, s = 0; i < mid; i++, s++) {
+            /*** wrong code: while(m<end && sums[m++]-sums[i]<lower);  ***/
+            while (m < end && sums[m] - sums[i] < lower) m++;
+            while (n < end && sums[n] - sums[i] <= upper) n++;
+            count += n - m;
+            /*** cache will merge-in-the-smaller-part-of-list2 ***/
+            while (t < end && sums[t] < sums[i]) cache[s++] = sums[t++];
+            cache[s] = sums[i];
+            len = s;
+        }
+
+        for (int i = 0; i <= len; i++) sums[start + i] = cache[i];
+        return count;
     }
 };
-
 int main() {
     return 0;
 }
