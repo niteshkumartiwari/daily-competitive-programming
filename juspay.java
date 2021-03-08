9256e3f3d375442874fc859e9e99dcba49b5d99f
niteshkumartiwari <niteshsoftwaredeveloper@gmail.com>
2021-03-08 16:41:36 +0530

    final-submission

--- juspay.java
+++ juspay.java
@@ -1,68 +1,67 @@
-Thread Safe
-Nitesh Kumar Tiwari
+Thread Safe Nitesh Kumar Tiwari
 
-
-
-class Node{
+class Node {
     private List<Node> child;
     private Node parent;
-    private boolean locked;
-    private AtomicInteger lockedChildNode;
-    private boolean inLockedProcess;
+    private volatile AtomicInteger locked;
+    private volatile AtomicInteger lockedChildNode;
 
-    Node(){
-        //Initialize child
-        this.child= new ArrayList<>();
-        this.parent= null;
-        this.locked = false;
-        this.lockedChildNode=0;
-        this.inLockedProcess=false;
-        this.childNodesInProgress=false;
+    Node() {
+        // Initialize child
+        this.child = new ArrayList<>();
+        this.parent = null;
+        this.locked = 0;
+        this.lockedChildNode = 0;
     }
+
 }
 
-public boolean lock(Node node){
-    if(node.isLocked()) return true;
+    public boolean lock(Node node) {
+        if (node.locked > 0)
+            return true;
 
-    //if some child is already locked
-    if(node.getLockedChildNode() > 0) return false;
+        node.locked++;// incr. first
+        if (node.lockedChildNode > 0)
+            return false;
 
-    //acquire lock for current node
-    while(node.inLockedProcess || node.childNodesInProgress);
-    node.inLockedProcess=true;
+        if (node.locked > 1) {// only allow first thread to enter
+            node.locked--;
+            return false;
+        }
 
-    //traverse upwards to check if any ancestors are locked
-    for(Node curr= node.getParent();curr!=null; curr= node.getParent()){
-        if(curr == null) break;
-        while(curr.inLockedProcess);
-        if(curr.isLocked()) return false;
-        curr.childNodesInProgress=true;
+        // traverse upwards to check if any ancestors are locked
+        Node parent;
+        for (parent = node.getParent(); parent != null && node.locked; parent = node.getParent()) {
+            if (parent == null)
+                break;
+
+            parent.lockedChildNode++;
+            if (parent.locked > 0) {// <-Td
+                node.locked = 0;
+                parent.lockedChildNode--;
+                break;
+            }
+        }
+
+        // traverse the ancestors to update the locked child counter till the last
+        // parent we traversed
+        for (Node curr = node.getParent(); curr != parent && !node.locked; curr = node.getParent()) {
+            if (curr == null)
+                break;
+            curr.lockedChildNode--;
+        }
+
+        return node.locked == 1;
     }
-
-    //traverse the ancestors to update the locked child counter
-    for(Node curr= node.getParent();curr!=null; curr= node.getParent()){
-        if(curr == null) break;
-        curr.incLockedChildNode(1);
-        curr.childNodesInProgress=false;
-    }
-    
-    node.setLocked(true);//lock the node
-    
-     //release the lock on current Node
-    node.inLockedProcess=false;
-
-    return true;
-}
 
 public boolean unlock(Node node){
     if(!node.isLocked()) return true;
 
-    node.setLocked(false);//unlock the node
+    node.locked=0;//unlock the node
     //traverse the ancestors to update the locked child counter
     for(Node curr= node.getParent();curr!=null; curr= node.getParent()){
         if(curr == null) break;
-        int val= curr.getLockedChildNode();
-        curr.setLockedChildNode(val-1);
+        curr.lockedChildNode--;
     }
 
     return true;
